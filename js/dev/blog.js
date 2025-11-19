a71acc8053c280778794610e285f939cd45eac04
Stalmax76 <68605477+Stalmax76@users.noreply.github.com>
2025-11-19 04:50:59 +0100

    update 19.11.25
    finished creating the main static pages of the site

---
+++ js/dev/blog.js
@@ -0,0 +1,16 @@
+import "./app.min.js";
+/* empty css          */
+/* empty css               */
+const workItems = document.querySelectorAll(".body-work__item");
+workItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    workItems.forEach((i) => {
+      i.classList.remove("item-content-work--active");
+    });
+    item.classList.add("item-content-work--active");
+  });
+});
+const workFooter = document.querySelector(".footer");
+if (workFooter) {
+  workFooter.classList.add("footer--simple");
+}
---
+++ js/dev/work.js
@@ -0,0 +1,12 @@
+import "./app.min.js";
+/* empty css          */
+/* empty css               */
+const workItems = document.querySelectorAll(".content-work__item");
+workItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    workItems.forEach((i) => {
+      i.classList.remove("item-content-work--active");
+    });
+    item.classList.add("item-content-work--active");
+  });
+});
